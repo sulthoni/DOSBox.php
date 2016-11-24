2091ca2e49fac729dad07b6dcd1a17655c21c523
sulthoni ashiddiiqi <sulthoni ashiddiiqi>
2016-11-24 15:48:51 +0700

    Merged time

--- app/DOSBox/Command/Library/CmdTime.php
+++ app/DOSBox/Command/Library/CmdTime.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace DOSBox\Command\Library;
@@ -50,52 +49,4 @@ class CmdTime extends Command {
 
     }
 
-=======
-<?php
-
-namespace DOSBox\Command\Library;
-
-use DOSBox\Interfaces\IDrive;
-use DOSBox\Interfaces\IOutputter;
-//use DOSBOx\Filesystem\Directory;
-use DOSBox\Command\BaseCommand as Command;
-
-class CmdTime extends Command {
-    //setting variabel untuk mengetahui
-    private $timeParams;
-
-    public function __construct($commandName, IDrive $drive){
-        parent::__construct($commandName, $drive);
-    }
-
-    public function checkNumberOfParameters($numberOfParametersEntered) {
-        return ($numberOfParametersEntered == 0 || $numberOfParametersEntered == 1);
-    }
-
-    public function checkParameterValues(IOutputter $outputter) {
-        if($this->getParameterCount() > 0) {
-            $this->timeParams = $this->params[0];
-            return true;
-        } else {
-            return true;
-        }
-    }
-
-    public function execute(IOutputter $outputter){
-        if($this->timeParams == "gaga") {
-            $outputter->printLine ("Command rejected, please input the right format");
-            $this->timeParams=null;
-        } elseif ($this->timeParams == "21:30:10"){
-        
-        } elseif ($this->timeParams==null) {
-            date_default_timezone_set("Asia/Jakarta");
-            $outputter->printLine (date("H:i:s"));
-        } else {
-            $outputter->printLine ("Command rejected, please input the right format");
-            $this->timeParams=null;
-        }
-
-    }
-    
->>>>>>> 27834af4208f071456ca588ba711449fc3def933
 }
