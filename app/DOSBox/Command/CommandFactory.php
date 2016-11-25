884148f0a893feecbfe6c3c317463a1e038540e5
sulthoni ashiddiiqi <sulthoni ashiddiiqi>
2016-11-25 15:54:32 +0700

    update command factory

--- app/DOSBox/Command/CommandFactory.php
+++ app/DOSBox/Command/CommandFactory.php
@@ -7,13 +7,10 @@ use DOSBox\Command\Library\CmdCd as CmdCd;
 use DOSBox\Command\Library\CmdDir as CmdDir;
 use DOSBox\Command\Library\CmdMkDir as CmdMkDir;
 use DOSBox\Command\Library\CmdMkFile as CmdMkFile;
-<<<<<<< HEAD
 use DOSBox\Command\Library\CmdTime as CmdTime;
 use DOSBox\Command\Library\CmdHelp as CmdHelp;
-=======
 use DOSBox\Command\Library\CmdVer as CmdVer;
-use DOSBox\Command\Library\CmdTime as CmdTime;
->>>>>>> dec3aa05a6653e8985cc0ada2816b256b1edb2e2
+
 
 class CommandFactory {
     private $commands = array();
@@ -28,10 +25,6 @@ class CommandFactory {
         array_push($this->commands, new CmdMkFile("mf", $drive));
         array_push($this->commands, new CmdTime("time", $drive));
         array_push($this->commands, new CmdVer("ver", $drive));
-
-
-        // Add your commands here
-        array_push($this->commands, new CmdTime("time", $drive));
         array_push($this->commands, new CmdHelp("help", $drive));
     }
 
